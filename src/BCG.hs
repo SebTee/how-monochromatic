4a58cf91072cce2877a5af87719b7fc4f1344c85
SebTee <seb.tom.tee@gmail.com>
2021-12-28 14:00:13 +0000

    Create BCG library

---
+++ src/BCG.hs
@@ -0,0 +1,40 @@
+-- | Library for handling bi-coloured graphs
+module BCG where
+
+import Data.Complex
+import qualified Data.IntSet as Set
+
+-- * Data Types
+
+-- | Bi-Coloured Graph
+data BCG
+    = BCG
+    [BCE]      -- ^ List of 'BCE's
+    Set.IntSet -- ^ Set of Vertices
+    Int        -- ^ Number of different colours
+
+-- | Bi-Coloured Edge
+data BCE = BCE { fromV :: Int            -- ^ From Vertex
+               , fromC :: Int            -- ^ From Colour
+               , toV   :: Int            -- ^ To Vertex
+               , toC   :: Int            -- ^ To Colour
+               , w     :: Complex Double -- ^ Weight
+}
+
+-- * Functions
+
+-- | Get a list of 'BCE's connected to a vertex
+getIncidentEdges :: BCG -> Int -> [BCE]
+getIncidentEdges (BCG es _ _) v = filter (isIncident v) es
+
+-- | Determines if a vertex is connected to an 'BCE'
+isIncident :: Int -> BCE -> Bool
+isIncident v BCE{fromV = from, toV = to} = (v == from) || (v == to)
+
+-- | Removes a vertex and the connected 'BCE's from a 'BCG'
+removeVertex :: BCG -> Int -> BCG
+removeVertex (BCG es vs c) v = BCG (filter (\e -> not $ v `isIncident` e) es) (Set.delete v vs) c
+
+-- | Removes a list of vertices and their connected 'BCE's from a 'BCG'
+removeVertices :: BCG -> [Int] -> BCG
+removeVertices = foldl removeVertex
