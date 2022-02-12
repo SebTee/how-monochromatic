a9cbda81800b621ae2e2c04e4c4f3b87f0e69dfe
SebTee <seb.tom.tee@gmail.com>
2022-02-12 15:40:13 +0000

    Create enumeratePM function and test it

--- src/BCG.hs
+++ src/BCG.hs
@@ -22,7 +22,23 @@ data BCE = BCE { fromV :: Int            -- ^ From Vertex
                , w     :: Complex Double -- ^ Weight
 } deriving Show
 
+-- * Constructors
+
+-- | A 'BCG' with no edges or vertices and the number of different colors is 0
+empty :: BCG
+empty = BCG [] Set.empty 0
+
 -- * Functions
+
+-- | Get a list of perfect matchings on a 'BCG'
+-- The perfect matchings are represented by the 'BCG' data type
+enumeratePM :: BCG -> [BCG]
+enumeratePM (BCG (e:es) vs c) = (if isPM then [BCG [e] vs c] else map addMatchingBack $ enumeratePM newSubG ) ++ enumeratePM (BCG es vs c)
+    where 
+        isPM = case e of BCE{fromV = from, toV = to} -> Set.fromList [from, to] == vs
+        newSubG = removeEdgeAndIncidentVertices (BCG (e:es) vs c) e
+        addMatchingBack (BCG mes mvs _) = case e of BCE{fromV = from, toV = to} -> BCG (e:mes) (Set.insert from $ Set.insert to mvs) c 
+enumeratePM _ = []
 
 -- | Get a list of 'BCE's connected to a vertex
 getIncidentEdges :: BCG -> Int -> [BCE]
@@ -31,6 +47,10 @@ getIncidentEdges (BCG es _ _) v = filter (isIncident v) es
 -- | Determines if a vertex is connected to an 'BCE'
 isIncident :: Int -> BCE -> Bool
 isIncident v BCE{fromV = from, toV = to} = (v == from) || (v == to)
+
+-- | Removes a 'BCE' from a 'BCG' and the vertices it was incident to
+removeEdgeAndIncidentVertices :: BCG -> BCE -> BCG
+removeEdgeAndIncidentVertices g BCE{fromV = from, toV = to} = removeVertices g [from, to]
 
 -- | Removes a vertex and the connected 'BCE's from a 'BCG'
 removeVertex :: BCG -> Int -> BCG
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 import Test.Hspec
 import ParseBCG
 import Data.Either
+import qualified Data.IntSet as Set
 import BCG
 
 validBCG = "1 green 2 green 1 0 \n\
@@ -15,6 +16,8 @@ validBCG = "1 green 2 green 1 0 \n\
            \4 red 6 green 0 1 \n\
            \4 blue 6 blue 1 0 \n\
            \5 green 6 green 1 0"
+
+parsedBCG = fromRight (BCG [] Set.empty 0) $ parse validBCG
 
 invalidComplexNumBCG = "1 green 2 green 1 0 \n\
                        \1 blue 3 blue 1 0 \n\
@@ -71,3 +74,14 @@ main = hspec $ do
             it "returns an IncorrectNumberOfParameters exception and the first line the error occurred" $ do
                 show (parse invalidTooFewParamBCG) `shouldBe` show (Left (IncorrectNumberOfParameters, 2) :: Either (ParseException, Int) BCG)
 
+    describe "enumeratePM" $ do
+        context "when a graph with 8 perfect matchings is passed in" $ do
+            it "returns a list of 8 perfect matchings" $ do
+                length (enumeratePM parsedBCG) `shouldBe` 8
+            it "returns a list of perfect matchings where each perfect matching has the same set of vertices as the original graph" $ do
+                parsedBCG `shouldSatisfy` \(BCG _ vs _) -> foldr (&&) True $ map (\(BCG _ mvs _) -> mvs == vs) $ enumeratePM parsedBCG
+            it "returns a list of perfect matchings where each perfect matching has n/2 edges where n is the number of vertices" $ do
+                parsedBCG `shouldSatisfy` \(BCG _ vs _) -> foldr (&&) True $ map (\(BCG mes _ _) -> length mes == Set.size vs `div` 2) $ enumeratePM parsedBCG
+        context "when an empty graph is passed in" $ do
+            it "returns an empty list" $ do
+                length (enumeratePM BCG.empty) `shouldBe` 0
