efd98e1c7203f7d95790bf652294f23d41be78a7
SebTee <seb.tom.tee@gmail.com>
2022-02-19 15:25:10 +0000

    read graph from stdin.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
 import BCG
+import ParseBCG
+import Data.Either
+import System.IO
+import qualified Data.IntSet as Set
+import System.Exit
 
 main :: IO ()
-main = putStr ""
+main = do
+    contents <- getContents
+    case parse contents of
+        Right g -> print $ enumeratePM g
+        Left (e, l) -> case e of 
+            IncorrectNumberOfParameters -> die $ errMsg "Incorrect number of parameters" l contents
+            ComplexNumberParseFailure -> die $ errMsg "Failed to parse the complex number" l contents
+    where 
+        errMsg m l c = m ++ " on line " ++ (show l) ++ "\n\n" ++ lines c !! (l - 1)
