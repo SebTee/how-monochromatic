4af952252aeacca94461350f4e1cc7d302789a1e
SebTee <seb.tom.tee@gmail.com>
2022-02-23 14:06:07 +0000

    Removed redundant brackets

--- app/Main.hs
+++ app/Main.hs
@@ -12,8 +12,8 @@ main = do
     contents <- getContents
     case parse contents of
         Right g -> print $ dist g
-        Left (e, l) -> case e of 
+        Left (e, l) -> case e of
             IncorrectNumberOfParameters -> die $ errMsg "Incorrect number of parameters" l contents
             ComplexNumberParseFailure -> die $ errMsg "Failed to parse the complex number" l contents
-    where 
-        errMsg m l c = m ++ " on line " ++ (show l) ++ "\n\n" ++ lines c !! (l - 1)
+    where
+        errMsg m l c = m ++ " on line " ++ show l ++ "\n\n" ++ lines c !! (l - 1)
